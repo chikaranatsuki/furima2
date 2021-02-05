b54fc56c324d2927be88b9d1f82881510708f60b
chikaranatsuki <69283318+chikaranatsuki@users.noreply.github.com>
2021-02-05 20:16:55 +0900

    token取得

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,16 +1,20 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item
+  before_action :move_to_index
+
   def index
-    @item = Item.find(params[:item_id])
     @order = PayForm.new
   end
 
   def create
     @order = PayForm.new(order_params)
+    # binding.pry
     if @order.valid?
+      pay_item
       @order.save
       redirect_to root_path
     else
-      @item = Item.find(params[:item_id])
       render 'index'
     end
   end
@@ -25,6 +29,23 @@ class OrdersController < ApplicationController
       :address,
       :building,
       :phone_number
-    ).merge(user_id: current_user.id, item_id: params[:item_id])
+    ).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: "jpy"
+    )
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def move_to_index
+    return redirect_to root_path if current_user.id == @item.user.id
   end
 end
--- app/models/pay_form.rb
+++ app/models/pay_form.rb
@@ -1,7 +1,7 @@
 class PayForm
   include ActiveModel::Model
 
-  attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :user_id, :item_id
+  attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :user_id, :item_id, :token
   
   with_options presence: true do
     validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/}
@@ -11,6 +11,7 @@ class PayForm
     validates :phone_number
     validates :user_id
     validates :item_id
+    validates :token
   end
 
   def save
