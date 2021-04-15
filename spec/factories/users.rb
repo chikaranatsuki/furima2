5bb2a6f11c10805a10f416882388b0972b7ede80
chikaranatsuki <69283318+chikaranatsuki@users.noreply.github.com>
2021-04-15 21:46:36 +0900

    webサーバーを設定しよう

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,8 @@ FactoryBot.define do
     nickname { Faker::Internet.username }
     password { '1a' + Faker::Internet.password(min_length: 7, max_length: 20) }
     email { Faker::Internet.free_email}
-    birth_date { Faker::Date.between_except(from: 20.year.ago, to: 1.year.from_now, excepted: Date.today) }
+    # birth_date { Faker::Date.between_except(from: 20.year.ago, to: 1.year.from_now, excepted: Date.today) }
+    birth_date {'2020-01-01'}
     first_name {'山田'}
     last_name {'太郎'}
     first_name_kana {'ヤマダ'}
